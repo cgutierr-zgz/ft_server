f77727d444e195f1686f28abfc72d4456c51ef62
Carlos Gutierrez moreno <cgutierr@c2r15s6.42madrid.com>
2021-05-17 20:15:16 +0200

    DONETE

--- srcs/utils/autoindex.sh
+++ srcs/utils/autoindex.sh
@@ -4,7 +4,7 @@ if [[ "$1" == "on" ]]
 then
 sed 's/off;#autoindex/on;#autoindex/g' /etc/nginx/sites-enabled/config_on.conf > /etc/nginx/sites-enabled/change.conf 
 mv etc/nginx/sites-enabled/change.conf etc/nginx/sites-enabled/config_on.conf
-mv /var/www/html/index.nginx-debian.html /var/www/html/wordpress/tmp/index.nginx-debian.off 2> /dev/null # Para suprimir warnings de que no existe
+mv /var/www/html/index.nginx-debian.html /var/www/html/wordpress/tmp/index.nginx-debian.off #2> /dev/null # Para suprimir warnings de que no existe
 service nginx restart
 fi
 
